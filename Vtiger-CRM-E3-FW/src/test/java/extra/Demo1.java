54c8af902040c46c63f84732678db00c3e79b947
AutomationWithPiyush <automation.with.piyush@gmail.com>
2025-07-24 13:29:38 +0530

    Helper Attributes DONE

--- Vtiger-CRM-E3-FW/src/test/java/extra/Demo1.java
+++ Vtiger-CRM-E3-FW/src/test/java/extra/Demo1.java
@@ -1,15 +1,21 @@
 package extra;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Demo1 {
 
-	@Test
-	public void add() {
-		System.out.println("Hey there!!!");
-		Reporter.log("Hello world!!!",true);
+	@Test(invocationCount = 10,threadPoolSize = 5)
+	public void create() throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		Reporter.log("Create Noida",true);
+		driver.quit();
 	}
 	
-
+	@Test(enabled = false)
+	public void delete() {
+		Reporter.log("Delete Noida",true);
+	}
 }
