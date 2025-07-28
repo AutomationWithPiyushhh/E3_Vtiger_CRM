473d80cb45b502cd2db9c6cda353a46f555df41f
AutomationWithPiyush <automation.with.piyush@gmail.com>
2025-07-28 13:36:27 +0530

    Types of Execution

--- Vtiger-CRM-E3-FW/src/test/java/extra/TwoDimension.java
+++ Vtiger-CRM-E3-FW/src/test/java/extra/TwoDimension.java
@@ -1,12 +1,38 @@
 package extra;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TwoDimension {
-	@Test
-	public Object[][] stringArray() {
-		Object[][] students = new Object[5][2];
-		
+
+	@Test(dataProvider = "getData")
+	public void loginToFB(String user, String password) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+		driver.get("https://www.facebook.com/");
+
+//		String user = "Shivang";
+		driver.findElement(By.id("email")).sendKeys(user);
+		driver.findElement(By.id("pass")).sendKeys(password);
+
+		driver.findElement(By.name("login")).click();
+
+		System.out.println(user + " is Logged in successfully");
+
+		driver.quit();
+	}
+
+	@DataProvider
+	public Object[][] getData() {
+		Object[][] students = new Object[3][2];
+
 		students[0][0] = "Mayank";
 		students[0][1] = "Verma";
 
@@ -16,12 +42,6 @@ public class TwoDimension {
 		students[2][0] = "Owias";
 		students[2][1] = "Thakur";
 
-		students[3][0] = "Pankaj";
-		students[3][1] = "Upadhyay";
-
-		students[4][0] = "Saloni";
-		students[4][1] = "V";
-		
 		return students;
 	}
 }
